62787456b789ea584f04d4f3b3c39c945522c214
Jonas Haffky <jonas.david.benno.haffky@uni-weimar.de>
2017-06-06 11:54:55 +0200

    add 4.3 todo error messages

--- source/list.hpp
+++ source/list.hpp
@@ -51,10 +51,153 @@ public:
 
     friend class ListIterator<T>;
     friend class ListConstIterator<T>;
-    // Aufgabe 2 begin
+    
+    // Aufgabe 4.2 
     List():m_first(nullptr),m_last(nullptr),m_size(0){};
     bool empty() const{return m_first == nullptr && m_last == nullptr;}
     std::size_t size() const{return m_size;}
+    
+    //Aufgabe 4.3
+    void push_front(value_type value) //pushes element to front of list
+	{
+		//Case List empty
+		if (m_size == 0)
+		{
+			//m_first zeigt auf das neue element
+			m_first = new ListNode<T>{
+				value, 		//T
+				nullptr, 	//pointer auf prev
+				nullptr 	//pointer auf next
+			};
+
+			//m_first und m_last zeigen auf das
+			//einzige Element der Liste
+			m_last = m_first;
+		}
+
+		//Case List not empty
+		else
+		{
+			//former_first und m_first referenzieren
+			//den front node
+			ListNode<T>* former_first = m_first;
+
+			//ListNode<T>* m_first referenziert den
+			//neuen front node, dessen next der former_
+			m_first = new ListNode<T>{
+				value, 			//T
+				nullptr, 		//pointer auf prev
+				former_first 	//pointer auf next
+			};
+
+			//prev pointer des 2. elements
+			//zeigt jetzt auf das front element
+			former_first->m_prev = m_first;
+		}
+
+		++ m_size;
+	}
+
+	void push_back(value_type value) //pushs element to back of list
+	{
+		//Case List empty
+		if (m_size == 0)
+		{
+			push_front(value);
+		}
+
+		//Case List not empty
+		else
+		{
+			ListNode<T>* former_last = m_last;
+
+			m_last = new ListNode<T>{
+				value, former_last, nullptr};
+
+			former_last->m_next = m_last;
+
+			++ m_size;
+		}
+	}
+
+	value_type pop_front()//removes front element of list and returns it's value
+	{
+		//Case List empty
+		if (m_size == 0)
+		{
+			//ERROR???
+		}
+		//Case List has one element
+		else if (m_size == 1)
+		{
+			//erstes element zwischenspeichern
+			ListNode<T>* former_first = m_first;
+			
+			m_first = nullptr;
+			m_last = nullptr;
+
+			--m_size;
+			return former_first->m_value;
+			
+		}
+		//Case List has multiple elements
+		else
+		{				
+			//erstes element zwischenspeichern
+			ListNode<T>* former_first = m_first;
+			
+			//front zeigt auf das element auf das next
+			//des ehm. front auch zeigt
+			m_first = former_first->m_next;
+
+			//das prev element von front wird auf nullptr umgebogen
+			m_first->m_prev = nullptr;
+            -- m_size;
+			return former_first->m_value;
+		}
+	}
+	
+	
+	value_type pop_back() //removes front element of list and returns it's value
+	{
+		//Case List empty
+		if (m_size == 0)
+		{
+			//ERROR???
+		}
+		//Case List has one element
+		else if (m_size == 1)
+		{
+			return pop_front();	
+		}
+		//Case List has multiple elements
+		else
+		{				
+			//erstes element zwischenspeichern
+			ListNode<T>* former_last = m_last;
+			
+			//front zeigt auf das element auf das next
+			//des ehm. front auch zeigt
+			m_last = former_last->m_prev;
+
+			//das prev element von front wird auf nullptr umgebogen
+			m_last->m_next = nullptr;
+            -- m_size;
+			return former_last->m_value;
+		}
+	}
+	
+	//returns value of front element
+	value_type front() const
+	{
+		return m_first->m_value;
+	}
+	
+	//returns value of back element
+	value_type back() const
+	{
+		return m_last->m_value;
+}
 private:
     std::size_t m_size = 0;
     ListNode<T>* m_first = nullptr;
--- source/testlist.cpp
+++ source/testlist.cpp
@@ -16,6 +16,60 @@ TEST_CASE("size", "[List]")
 	REQUIRE(l1.size()==0);
 }
 
+// AUFGABE4.3
+
+TEST_CASE("add an element with push_front", "[modifiers]")
+{
+	List<int> l2;
+	l2.push_front(19);
+	REQUIRE(l2.front()==19);
+}
+
+TEST_CASE("add an element with push_back", "[modifiers]")
+{
+	List<int> list;
+	list.push_back(54);
+	REQUIRE(list.back()==54);
+}
+
+TEST_CASE("remove an element with pop_front", "[modifiers]")
+{
+	List<int> list;
+	list.push_front(42);
+	REQUIRE(42 == list.front());
+	REQUIRE(42 == list.back());
+	
+	list.push_front(12);
+
+	REQUIRE(12 == list.front());
+	REQUIRE(42 == list.back());
+
+	list.pop_front();
+	REQUIRE(42 == list.front());
+
+	list.pop_front();
+	REQUIRE(list.size() == 0);
+}
+
+TEST_CASE("remove an element with pop_back", "[modifiers]")
+{
+	List<int> list;
+	list.push_front(42);
+	REQUIRE(42 == list.front());
+	REQUIRE(42 == list.back());
+	
+	list.push_front(12);
+
+	REQUIRE(12 == list.front());
+	REQUIRE(42 == list.back());
+
+	list.pop_back();
+	REQUIRE(12 == list.front());
+
+	list.pop_back();
+	REQUIRE(list.size() == 0);
+}
+
 int main(int argc, char* argv[])
 {
     return Catch::Session().run(argc, argv);
